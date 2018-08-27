0d7f0936bc361980db227f38188d11c02aad262b
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2018-08-27 10:54:36 -0700

    Xcode project

---
+++ bus20/bus20/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bus20
+//
+//  Created by SATOSHI NAKAJIMA on 8/27/18.
+//  Copyright © 2018 SATOSHI NAKAJIMA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
