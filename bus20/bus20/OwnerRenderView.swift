ba60988509f96461dc1593a07ed69d6578f8707d
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2018-09-29 22:53:46 -0700

    OwnerRenderView

---
+++ bus20/bus20/OwnerRenderView.swift
@@ -0,0 +1,22 @@
+//
+//  OwnerRenderView.swift
+//  bus20
+//
+//  Created by SATOSHI NAKAJIMA on 9/29/18.
+//  Copyright © 2018 SATOSHI NAKAJIMA. All rights reserved.
+//
+
+import UIKit
+
+protocol OwnerRenderViewDelegate:NSObjectProtocol {
+    func draw(_ rect:CGRect)
+}
+class OwnerRenderView: UIView {
+    weak var delegate:OwnerRenderViewDelegate?
+    required init?(coder:NSCoder) {
+        super.init(coder: coder)
+    }
+    override func draw(_ rect: CGRect) {
+        delegate?.draw(rect)
+    }
+}
